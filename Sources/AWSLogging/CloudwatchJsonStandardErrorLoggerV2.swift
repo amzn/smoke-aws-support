ea52a629dc554a9154c8bd605cfbfbdc594c5ffb
Simon Pilkington <simonpi@amazon.com>
2023-01-18 03:48:07 +1100

    Add actually correct header.

--- Sources/AWSLogging/CloudwatchJsonStandardErrorLoggerV2.swift
+++ Sources/AWSLogging/CloudwatchJsonStandardErrorLoggerV2.swift
@@ -11,7 +11,7 @@
 // express or implied. See the License for the specific language governing
 // permissions and limitations under the License.
 //
-// CloudWatchPendingMetricsQueueV2.swift
+// CloudwatchJsonStandardErrorLoggerV2.swift
 // AWSLogging
 //
 
